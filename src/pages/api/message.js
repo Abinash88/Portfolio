3c478db8a56ad15e28018c4f43c00cd3c5b642bd
Abinash <subediabinas@gmail.com>
2023-07-05 08:29:26 +0100

    managing error

--- src/pages/api/message.js
+++ src/pages/api/message.js
@@ -1,11 +1,12 @@
 import SendMail from "./Email";
 
 const handler = async(req, res) => {
-    // if(req.method !== 'POST') return res.status(405).json({success:false, message:' Post method only allowed'})
+    const bodys = req.body
+    if(req.method !== 'POST') return res.status(405).json({success:false, message:' Post method only allowed',bodys})
     const {firstname, lastname, email, subject, message} = req.body;
 
-    if(!firstname || !lastname || !email || !subject || !message) return res.status(401).json({success:false, message:'Please fill in all fields'});
-    console.log(firstname, email)
+    if(!firstname || !lastname || !email || !subject || !message) return res.status(401).json({success:false, message:'Please fill in all fields', bodys});
+    console.log(firstname, lastname, email, subject, message)
     try{
         SendMail(firstname, lastname, email, subject, message);
     }catch(err) {
